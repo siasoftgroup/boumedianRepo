98ae133d3e2118856571cac98b0c224f1a3f6597
Boussaid <Boussaid@192.168.1.10>
2018-02-11 14:57:33 +0100

    bou

--- labs-module01/src/siasoft/module1/main.java
+++ labs-module01/src/siasoft/module1/main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
  */
 public class main {
 
-	
 	/**
 	 * @param args
 	 * @throws ClassNotFoundException
@@ -17,27 +16,31 @@ public class main {
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-
+		//verifier si un nombre est pair 
+		test t = new test();
+		t.verifierCode();
+		System.out.println("*********************************************");
 		automobile auto = new automobile("wolswaguen", "rouge", 320);
+		System.out.println("*********************************************");
 		auto.toString();
-
+		System.out.println("*********************************************");
 		/* instanciation */
 		filtre f = new filtre(" fiat", "bleu", 220, "ga620", "m605", "z213");
-
+		System.out.println("*********************************************");
 		/* surcharge */
 		System.out.println(f.affiche());
 		System.out.println(f.affiche(" misfat"));
-
+		System.out.println("*********************************************");
 		/* recuperation des donnees apartir de BD */
 		f.user();
-		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Saisissez nom client : ");
-		    String nom = sc.nextLine();
-		    System.out.println("Saisissez adresse client : ");
-		    String adr = sc.nextLine();
-		    System.out.println("Saisissez nemero de telephone client : ");
-		    int tel = sc.nextInt();
-		    f.ajoutuser(nom,adr,tel);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Saisissez nom client : ");
+		String nom = sc.nextLine();
+		System.out.println("Saisissez adresse client : ");
+		String adr = sc.nextLine();
+		System.out.println("Saisissez nemero de telephone client : ");
+		int tel = sc.nextInt();
+		f.ajoutuser(nom, adr, tel);
 	}
 
 }
---
+++ labs-module01/src/siasoft/module1/test.java
@@ -0,0 +1,28 @@
+package siasoft.module1;
+
+import java.util.Scanner;
+
+public class test {
+
+	/**
+	 * Method permet de faire ....
+	 * 
+	 * @return boolean si égale à 50
+	 */
+	public void verifierCode() {
+
+		
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Saisissez un chiffre a verifier : ");
+		    int i = sc.nextInt();
+		    System.out.println("liste de nombre pair inclu ");
+		for (int j = 1; j < i; j++) {
+			if (j % 2 == 0) {
+				System.out.println( j + ",");
+
+			} 
+		}
+
+	}
+
+}
